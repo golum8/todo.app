33b9836d3643a75d3d46d1cc8c85feecf1dd0d9c
Artur Kubiak <artur@arkemdeia.pl>
2022-12-11 21:28:17 +0100

    Zmieniono pom.xml oraz zastąpiono javax przez jakarta

--- src/main/java/io/github/golum8/todo/app/model/Task.java
+++ src/main/java/io/github/golum8/todo/app/model/Task.java
@@ -1,8 +1,8 @@
 package io.github.golum8.todo.app.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
